72a54ae88d124bc921f7436115e99f35e220e2d7
muso1223 <yulianmaldonado1223@gmail.com>
2025-11-24 21:49:41 -0500

    Update target

---
+++ target/classes/data.sql
@@ -0,0 +1,122 @@
+-- USUARIOS
+INSERT INTO usuarios (tdoc, numdoc, papellido, sapellido, pnomb, snomb, correo, password, telefono, rol, registro)
+VALUES 
+    -- ADMIN
+('CC', '52325393', 'Ochoa', 'Vesga', 'Alba', 'Nery', 'admin@correo.com', '12345', '3162365932', 'ADMIN', NULL),
+    
+    -- COORDINADOR
+('CC', '1102354511', 'Maldonado', 'Ochoa', 'Breggie', 'Yulian', 'coordinador@correo.com', '12345', '3105679305', 'COORDINADOR', NULL),
+    
+    -- ALUMNOS
+('CC', '1102100500', 'Barbosa', 'Perez', 'Juan', 'Carlos', 'carlos@correo.com', '12345', '3101126958', 'ALUMNO', 'EK20183007722'),
+('CC', '1102100501', 'Quintero', 'Oviedo', 'Juan', 'Daniel', 'juan@correo.com', '12345', '3101126959', 'ALUMNO', 'EK20183140703'),
+('CC', '1102100502', 'Parra', 'Ochoa', 'Luis', 'Andrés', 'luis@correo.com', '12345', '3101126960', 'ALUMNO', 'EK20183040545'),
+('CC', '1102100503', 'Anaya', 'Ruiz', 'Juan', 'Diego', 'diego@correo.com', '12345', '3101126961', 'ALUMNO', 'EK20183025381'),
+('CC', '1102100504', 'Flor', 'Díaz', 'Daniel', 'Felipe', 'pipe@correo.com', '12345', '3101126962', 'ALUMNO', 'EK20183025335'),
+('CC', '1102100505', 'García', 'Torres', 'Juan', 'Pablo', 'juanp@correo.com', '12345', '3101126963', 'ALUMNO', 'EK20183122648'),
+('CC', '1102100506', 'Manosalva', 'Sarmiento', 'Miguel', 'Ángel', 'miguel@correo.com', '12345', '3101126964', 'ALUMNO', 'EK20183064605'),
+('CC', '1102100507', 'Mendoza', 'Rojas', 'Manuel', 'Santiago', 'manuel@correo.com', '12345', '3101126965', 'ALUMNO', 'EK20183187351'),
+('CC', '1102100508', 'Beltrán', 'Alvarado', 'Carlos', 'Andrés', 'carlosa@correo.com', '12345', '3101126966', 'ALUMNO', 'EK20183233820'),
+('CC', '1102100509', 'Santamaría', 'Mendoza', 'María', 'Andrea', 'maria@correo.com', '12345', '3101126967', 'ALUMNO', 'EK20183030016'),
+('CC', '1102100510', 'Sánchez', 'Rodríguez', 'Karen', 'Lorena', 'karen@correo.com', '12345', '3101126968', 'ALUMNO', 'EK20183047073'),
+('CC', '1102100511', 'Romero', 'Martínez', 'Nixon', 'Steven', 'nixon@correo.com', '12345', '3101126969', 'ALUMNO', 'EK20183236451'),
+('CC', '1102100512', 'Luna', 'Gómez', 'Roque', 'Julio', 'roque@correo.com', '12345', '3101126970', 'ALUMNO', 'EK20183041714'),
+('CC', '1102100513', 'Triana', 'Parra', 'Mario', 'Andrés', 'mario@correo.com', '12345', '3101126971', 'ALUMNO', 'EK20183187801'),
+('CC', '1102100514', 'Suárez', 'Chaparro', 'Julian', 'Andrés', 'juli@correo.com', '12345', '3101126972', 'ALUMNO', 'EK20183176566'),
+('CC', '1102100515', 'García', 'Sánchez', 'Mateo', 'Felipe', 'mateo@correo.com', '12345', '3101126973', 'ALUMNO', 'EK20183204427'),
+('CC', '1102100516', 'Pinzón', 'González', 'Julian', 'Barney', 'julian@correo.com', '12345', '3101126974', 'ALUMNO', 'EK20183196280'),
+('CC', '1102100517', 'Jaimes', 'Torres', 'Edgar', 'Fabián', 'edgar@correo.com', '12345', '3101126975', 'ALUMNO', 'EK20183173799'),
+('CC', '1102100518', 'Niño', 'Torreja', 'Fabian', 'Eduardo', 'fabio@correo.com', '12345', '3101126976', 'ALUMNO', 'EK20183009565'),
+('CC', '1102100519', 'Fabián', 'Bolívar', 'Juan', 'Alberto', 'beto@correo.com', '12345', '3101126977', 'ALUMNO', 'EK20183117756'),
+('CC', '1102100520', 'Hernández', 'Uribe', 'Ruben', 'Darío', 'ruben@correo.com', '12345', '3101126978', 'ALUMNO', 'EK20183044579'),
+('CC', '1102100521', 'Larios', 'Pardo', 'Luz', 'Amparo', 'luz@correo.com', '12345', '3101126979', 'ALUMNO', 'EK20183045760'),
+('CC', '1102100522', 'Calderón', 'Urrea', 'Alba', 'Nery', 'alba@correo.com', '12345', '3101126980', 'ALUMNO', 'EK20183034044'),
+('CC', '1102100523', 'Villarreal', 'Velandia', 'Lessly', 'Stephania', 'lessly@correo.com', '12345', '3101126981', 'ALUMNO', 'EK20183041521'),
+('CC', '1102100524', 'Restrepo', 'Hooker', 'Manuel', 'Adrián', 'manu@correo.com', '12345', '3101126982', 'ALUMNO', 'EK20183027436'),
+('CC', '1102100525', 'Cáceres', 'Rojas', 'Sergio', 'Adrián', 'sergio@correo.com', '12345', '3101126983', 'ALUMNO', 'EK20183031592'),
+('CC', '1102100526', 'Tabares', 'Polo', 'Hugo', 'Andrés', 'andresh@correo.com', '12345', '3101126984', 'ALUMNO', 'EK20183004153'),
+('CC', '1102100527', 'Naranjo', 'Hinestroza', 'Martín', 'Andrés', 'mart@correo.com', '12345', '3101126985', 'ALUMNO', 'EK20183030783'),
+('CC', '1102100528', 'Prada', 'Oviedo', 'Juan', 'Carlos', 'jc@correo.com', '12345', '3101126986', 'ALUMNO', 'EK20183024754'),
+('CC', '1102100529', 'Vargas', 'Ortiz', 'Luis', 'Miguel', 'lm@correo.com', '12345', '3101126987', 'ALUMNO', 'EK20183186200'),
+('CC', '1102100530', 'Torres', 'Ardila', 'Ana', 'Gabriel', 'ag@correo.com', '12345', '3101126988', 'ALUMNO', 'EK20183182410'),
+('CC', '1102100531', 'Ortiz', 'Mejía', 'Samuel', 'Andrés', 'sa@correo.com', '12345', '3101126989', 'ALUMNO', 'EK20183213735'),
+('CC', '1102100532', 'Villamizar', 'Rangel', 'Andrés', 'Antonio', 'anda@correo.com', '12345', '3101126990', 'ALUMNO', 'EK20183065220'),
+('CC', '1102100533', 'Restrepo', 'Gutiérrez', 'María', 'Valentina', 'marva@correo.com', '12345', '3101126991', 'ALUMNO', 'EK20183028123'),
+('CC', '1102100534', 'Higuera', 'Correa', 'Jana', 'Danisa', 'higuera@correo.com', '12345', '3101126992', 'ALUMNO', 'EK20183207870'),
+('CC', '1102100535', 'Matiz', 'Macareo', 'Nicole', 'Mariane', 'nicole@correo.com', '12345', '3101126993', 'ALUMNO', 'EK20183144329');
+
+-- EXAMENES INICIALES
+INSERT INTO examenes (usuario_id, puntajetotal, anulado, comunicacion, razonamiento, lectura, ciudadana, ingles, proyectos, pcientifico, dsoft, nivelingles)
+VALUES
+-- Juan Carlos Barbosa
+(3, 200, FALSE, 128, 182, 202, 206, 183, 185, 160, 197, 'B1'),
+-- Juan Daniel Quintero
+(4, 165, FALSE, 125, 151, 179, 163, 205, 182, 144, 136, 'B2'),
+-- Luis Andrés Parra
+(5, 164, FALSE, 159, 172, 182, 142, 165, 167, 132, 148, 'A2'),
+-- Juan Diego Anaya
+(6, 160, FALSE, 146, 199, 157, 149, 147, 174, 127, 171, 'A2'),
+-- Daniel Felipe Flor
+(7, 160, FALSE, 198, 153, 147, 157, 146, 168, 114, 160, 'A2'),
+-- Juan Pablo García
+(8, 157, FALSE, 179, 172, 158, 140, 136, 128, 121, 142, 'A1'),
+-- Miguel Ángel Manosalva
+(9, 153, FALSE, 115, 152, 159, 172, 165, 142, 118, 119, 'A2'),
+-- Manuel Santiago Mendoza
+(10, 151, FALSE, 132, 123, 125, 169, 204, 173, 127, 171, 'B2'),
+-- Carlos Andrés Beltrán
+(11, 150, FALSE, 86, 187, 160, 171, 148, 162, 125, 142, 'A2'),
+-- María Andrea Santamaría
+(12, 150, FALSE, 175, 149, 145, 158, 125, 162, 76, 125, 'A1'),
+-- Karen Lorena Sánchez
+(13, 149, FALSE, 209, 143, 117, 129, 147, 137, 125, 136, 'A2'),
+-- Nixon Steven Romero
+(14, 146, FALSE, 93, 183, 155, 164, 133, 174, 130, 154, 'A1'),
+-- Roque Julio Luna
+(15, 141, FALSE, 125, 157, 138, 135, 152, 176, 128, 165, 'A2'),
+-- Mario Andrés Triana
+(16, 141, FALSE, 150, 136, 145, 150, 126, 148, 129, 131, 'A1'),
+-- Julian Andrés Suárez
+(17, 140, FALSE, 128, 146, 146, 132, 147, 130, 110, 125, 'A2'),
+-- Mateo Felipe García
+(18, 139, FALSE, 129, 138, 148, 146, 135, 109, 107, 131, 'A1'),
+-- Julian Barney Pinzón
+(19, 138, FALSE, 153, 123, 127, 147, 140, 145, 143, 160, 'A1'),
+-- Edgar Fabián Jaimes
+(20, 137, FALSE, 166, 157, 124, 100, 140, 100, 105, 113, 'A1'),
+-- Fabian Eduardo Niño
+(21, 134, FALSE, 165, 137, 136, 118, 116, 146, 122, 154, 'A0'),
+-- Juan Alberto Fabián
+(22, 133, FALSE, 139, 93, 168, 150, 114, 102, 123, 94, 'A0'),
+-- Ruben Darío Hernández
+(23, 132, FALSE, 116, 166, 136, 104, 140, 158, 125, 154, 'A1'),
+-- Luz Amparo Larios
+(24, 131, FALSE, 149, 123, 129, 121, 131, 101, 102, 165, 'A1'),
+-- Alba Nery Calderón
+(25, 130, FALSE, 127, 147, 134, 111, 131, 65, 112, 94, 'A1'),
+-- Lessly Stephania Villarreal
+(26, 129, FALSE, 96, 162, 114, 131, 144, 122, 112, 131, 'A1'),
+-- Manuel Adrián Restrepo
+(27, 126, FALSE, 81, 134, 126, 149, 139, 127, 136, 142, 'A1'),
+-- Sergio Adrián Cáceres
+(28, 125, FALSE, 124, 135, 108, 92, 165, 132, 104, 131, 'A2'),
+-- Hugo Andrés Tabares
+(29, 124, FALSE, 131, 131, 107, 88, 162, 136, 112, 148, 'A2'),
+-- Martín Andrés Naranjo
+(30, 122, FALSE, 166, 113, 113, 112, 106, 135, 117, 119, 'A0'),
+-- Juan Carlos Prada
+(31, 122, FALSE, 119, 125, 137, 107, 123, 83, 104, 119, 'A1'),
+-- Luis Miguel Vargas
+(32, 114, FALSE, 95, 120, 151, 86, 119, 149, 103, 119, 'A0'),
+-- Ana Gabriel Torres
+(33, 113, FALSE, 109, 105, 104, 103, 142, 102, 135, 80, 'A1'),
+-- Samuel Andrés Ortiz
+(34, 107, FALSE, 128, 81, 107, 102, 119, 130, 111, 125, 'A0'),
+-- Andrés Antonio Villamizar
+(35, 106, FALSE, 134, 96, 92, 110, 97, 83, 107, 119, 'A0'),
+-- María Valentina Restrepo
+(36, 96, FALSE, 0, 117, 122, 105, 137, 157, 96, 131, 'A1'),
+-- Jana Danisa Higuera
+(37, NULL, TRUE, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+-- Nicole Mariane Matiz
+(38, NULL, TRUE, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
